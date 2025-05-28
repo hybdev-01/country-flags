d7786e6bbad5516b3eda87ff451c71ce2b195941
hybdev <hybdev.set@gmail.com>
2025-05-28 12:00:25 +0500

    change route paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Header } from './components/Header';
-import { Main } from './components/Main';
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
-import { HomePage } from './pages/HomePage';
-import { Details } from './pages/Details';
-import { NotFound } from './pages/NotFound';
+import { HomePage } from "./pages/HomePage";
+import { Details } from "./pages/Details";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -13,10 +13,8 @@ function App() {
       <Header />
       <Main>
         <Routes>
-          <Route path="/" element={
-            <HomePage />
-          } />
-          <Route path="/country/:name" element={<Details />} />
+          <Route path="/country-flags" element={<HomePage />} />
+          <Route path="/country-flags/country/:name" element={<Details />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Main>
